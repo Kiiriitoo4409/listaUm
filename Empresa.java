fe8028222fd8c8705ddbf44de56f50fc0494bbe1
Kiiriitoo4409 <168425327+Kiiriitoo4409@users.noreply.github.com>
2024-05-15 11:02:57 -0300

    Create Empresa.java

---
+++ Empresa.java
@@ -0,0 +1,27 @@
+package aulaExtra;
+
+public class Empresa {
+	private String nome;
+	private String cnpj;
+	
+	public Empresa(String novoNome, String novoCnpj) {
+		this.nome = novoNome;
+		this.cnpj = novoCnpj;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getCnpj() {
+		return cnpj;
+	}
+	
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+	
+	public void setCnpj(String novoCnpj) {
+		this.cnpj = novoCnpj;
+	}
+}
