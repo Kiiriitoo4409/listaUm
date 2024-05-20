3deef3719231783dc1c9c1b8a205495e5ab5859f
Kiiriitoo4409 <168425327+Kiiriitoo4409@users.noreply.github.com>
2024-05-20 09:10:08 -0300

    Disciplina.java

---
+++ Disciplina.java
@@ -0,0 +1,28 @@
+package aulaExtra;
+
+public class Disciplina {
+	private String nome;
+	private String codigo;
+	
+	public Disciplina(String novoNome, String novoCodigo) {
+	this.nome = novoNome;
+	this.codigo = novoCodigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+	
+	public void setCodigo(String novoCodigo) {
+		this.codigo = novoCodigo;
+	}
+
+}
